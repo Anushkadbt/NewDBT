16c55bd55d582eb29c5263c0086daf393e74a6fe
anushkadbt@gmail.com <anushkadbt@gmail.com>
2023-04-28 11:06:20 +0000

    finalscd1

--- models/SCD1/Dim_Account.sql
+++ models/SCD1/Dim_Account.sql
@@ -3,10 +3,9 @@
         materialized='incremental',
         incremental_strategy='delete+insert',
         unique_key = 'ACCOUNT_ID',
-        merge_update_columns = ['ACCOUNT_NAME','BILLING_CITY','SHIPPING_CITY','PHONE_NO']
+        merge_update_columns = ['ACCOUNT_NAME','BILLING_CITY','ACCOUNTNUMBER','PHONE_NO']
     )
 }}
-SELECT  40000+row_number() OVER (ORDER BY ID) AS ACCOUNT_KEY ,ID AS ACCOUNT_ID,ACCOUNTNUMBER AS ACCOUNT_NUMBER,
-        PHONE AS PHONE_NO ,NAME AS ACCOUNT_NAME,BILLINGCITY AS BILLING_CITY,
-        SHIPPINGCITY AS SHIPPING_CITY
+SELECT 0+row_number() OVER (ORDER BY ID) AS ACCOUNT_KEY ,ID AS ACCOUNT_ID,ACCOUNTNUMBER AS ACCOUNT_NUMBER,
+        PHONE AS PHONE_NO ,NAME AS ACCOUNT_NAME,BILLINGCITY AS BILLING_CITY
         from {{ source('stage_Account', 'ACCOUNT') }}
